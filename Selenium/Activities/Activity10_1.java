b41b784df3ce278e19c9b3ba9b01eec6a595cb3b
gokulsen <60639937+gokulsen@users.noreply.github.com>
2021-06-02 19:59:04 +0530

    Added Selenium Java activity

---
+++ Selenium/Activities/Activity10_1.java
@@ -0,0 +1,43 @@
+package activity;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Activity10_1 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Gokul\\Full Stack Tester\\Selenium\\Chrome Driver\\chromedriver.exe");  
+        WebDriver driver=new ChromeDriver();
+        Actions actions = new Actions(driver);
+
+        //Open browser
+        driver.get("https://www.training-support.net/selenium/input-events");
+        //Find the cude
+        WebElement cube = driver.findElement(By.id("wrapD3Cube"));
+
+        //Left click
+        actions.click(cube);
+        WebElement cubeVal = driver.findElement(By.className("active"));
+        System.out.println("Left Click: " + cubeVal.getText());
+
+        //Double click
+        actions.doubleClick(cube).perform();
+        cubeVal = driver.findElement(By.className("active"));
+        System.out.println("Double Click: " + cubeVal.getText());
+        
+        //Right click        
+        actions.contextClick(cube).perform();
+        cubeVal = driver.findElement(By.className("active"));
+        System.out.println("Right Click: " + cubeVal.getText());
+
+        //Close browser
+        driver.close();
+    }
+}
